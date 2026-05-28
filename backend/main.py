3db4f78057636de0fc65df32d5e68bf51fac6b36
NUELRL <nuelnneji335@gmail.com>
2026-05-28 12:50:55 +0100

    fix: update CORS middleware configuration

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,8 @@ app = FastAPI(title="Malaria Diagnosis API")
 # CORS (allow frontend access)
 app.add_middleware(
     CORSMiddleware,
-     allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
