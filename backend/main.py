1c56518336da6a93403d2945c1da2d9910b272cf
NUELRL <nuelnneji335@gmail.com>
2026-05-28 12:43:13 +0100

    fixed cors err

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Malaria Diagnosis API")
 # CORS (allow frontend access)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://.*\.vercel\.app"],
+    allow_origins_regex=["https://.*\.vercel\.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
